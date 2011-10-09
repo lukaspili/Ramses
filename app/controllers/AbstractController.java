89bdfeb24826e4b1bd5705ff7e874ef0b17bfcc8
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-10-09 17:20:35 +0200

    Add flash messages (success, error, info) to controller and template.

--- app/controllers/AbstractController.java
+++ app/controllers/AbstractController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import controllers.helper.PageHelper;
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.With;
 import validation.EnhancedValidator;
@@ -19,9 +20,15 @@ public abstract class AbstractController extends Controller {
         return new PageHelper(page, renderArgs);
     }
 
-    protected static void flashInfo(String message) {
-        flash.put("info", message);
+    protected static void flashInfo(String message, Object... args) {
+        flash.put("info_box", Messages.get(message, args));
     }
 
+    protected static void flashError(String message, Object... args) {
+        flash.put("error_box", Messages.get(message, args));
+    }
 
+    protected static void flashSuccess(String message, Object... args) {
+        flash.put("success_box", Messages.get(message, args));
+    }
 }
