89e1a5746d74566339a248a29711fcbde467a693
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-10-27 17:37:35 +0200

    Add collection helper to controllers.

--- app/controllers/helper/CollectionHelper.java
+++ app/controllers/helper/CollectionHelper.java
@@ -3,29 +3,41 @@ package controllers.helper;
 import play.classloading.enhancers.LocalvariablesNamesEnhancer;
 import play.db.jpa.Model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
  */
 public class CollectionHelper {
 
-    public <T extends Model> List<T> getFromIds(List<Long> ids) {
+    public <T extends Model> Set<T> getFromIds(Class<T> model, List<Long> ids) {
 
-        List<T> list = new ArrayList<T>();
+        Set<T> set = new HashSet<T>();
+
+        if (null == ids) {
+            return set;
+        }
 
         for (Long id : ids) {
 
-            T t = T.findById(id);
+            T t = Model.em().find(model, id);
 
             if (null != t) {
-                list.add(t);
+                set.add(t);
             }
         }
 
-        return list;
+        return set;
+    }
+
+    public <T extends Model> List<Long> getIdsFromModel(Set<T> models) {
+
+        List<Long> ids = new ArrayList<Long>();
+
+        for (T model : models) {
+            ids.add(model.getId());
+        }
+
+        return ids;
     }
 }
