375c5fbd79ba75da5cde9791654101a53c277d43
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-10-27 17:37:53 +0200

    Refactor user first login filter.

--- app/controllers/filters/UserFirstLoginFilter.java
+++ app/controllers/filters/UserFirstLoginFilter.java
@@ -3,6 +3,7 @@ package controllers.filters;
 import controllers.Dashboard;
 import controllers.security.Auth;
 import controllers.Users;
+import controllers.security.PublicAccess;
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -13,6 +14,14 @@ public class UserFirstLoginFilter extends Controller {
 
     @Before
     public static void checkFirstLogin() {
+
+        if (null != getActionAnnotation(PublicAccess.class)) {
+            return;
+        }
+
+        if (null == getActionAnnotation(UserFirstLoginOnly.class) && !Auth.getCurrentUser().active) {
+            Users.firstLogin();
+        }
 
         if (null != getActionAnnotation(UserFirstLoginOnly.class) && Auth.getCurrentUser().active) {
             Dashboard.index();
