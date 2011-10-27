35332a7a86fdb4a18e7bf36a895ec41d4308bb36
Lukasz Piliszczuk <lukasz.piliszczuk@zenika.com>
2011-10-27 17:38:41 +0200

    Refactor flash error to handle multiple messages.

--- app/controllers/abstracts/UtilController.java
+++ app/controllers/abstracts/UtilController.java
@@ -5,6 +5,7 @@ import controllers.filters.UserFirstLoginFilter;
 import controllers.helper.CollectionHelper;
 import controllers.helper.PageHelper;
 import controllers.security.Auth;
+import org.apache.commons.lang3.StringUtils;
 import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -16,9 +17,7 @@ import validation.EnhancedValidator;
 
 public abstract class UtilController extends Controller {
 
-    protected static CollectionHelper collectionHelper() {
-        return new CollectionHelper();
-    }
+    protected static CollectionHelper collectionHelper = new CollectionHelper();
 
     protected static EnhancedValidator validator() {
         return new EnhancedValidator(validation, params);
@@ -33,7 +32,28 @@ public abstract class UtilController extends Controller {
     }
 
     protected static void flashError(String message, Object... args) {
-        flash.put("error_box", Messages.get(message, args));
+        flashError(message, true, args);
+    }
+
+    protected static void flashErrorSamePage(String message, Object... args) {
+        flashError(message, false, args);
+    }
+
+    protected static void flashError(String message, boolean keep, Object... args) {
+
+        for (int i = 1; i <= 5; i++) {
+            String box = "error_box_" + i;
+
+            if (!flash.contains(box)) {
+                flash.put(box, Messages.get(message, args));
+
+                if (!keep) {
+                    flash.discard(box);
+                }
+
+                break;
+            }
+        }
     }
 
     protected static void flashSuccess(String message, Object... args) {
