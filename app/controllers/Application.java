382fc8730c14b7a7b03009ef3a94906e441e15b1
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2011-12-11 12:22:23 +0100

    About and reportbugs pages.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,24 +1,37 @@
 package controllers;
 
+import controllers.abstracts.AppController;
 import controllers.abstracts.UtilController;
+import controllers.helper.PageHelper;
 import controllers.security.LoggedAccess;
+import controllers.security.PublicAccess;
 import play.mvc.Before;
 import play.mvc.Controller;
 
 /**
  * @author Lukasz Piliszczuk <lukasz.piliszczuk AT zenika.com>
  */
-public class Application extends UtilController {
+@LoggedAccess
+public class Application extends AppController {
 
+    private static PageHelper pageHelper;
 
-
-    @LoggedAccess
-    public static void about() {
-
+    @Before
+    public static void before() {
+        pageHelper = new PageHelper("users", renderArgs);
     }
 
-    @LoggedAccess
+    public static void about() {
+
+        pageHelper.uniqueTitle("application.about");
+
+        render();
+    }
+
     public static void reportBug() {
 
+        pageHelper.uniqueTitle("application.reportBug");
+
+        render();
     }
 }
