fab04bd329e8e1027df061ae3e339b207b4799c3
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2011-12-13 15:44:34 +0100

    Init contract regeneration and fix invalid sta numbers.

--- app/controllers/Init.java
+++ app/controllers/Init.java
@@ -3,11 +3,13 @@ package controllers;
 import controllers.abstracts.UtilController;
 import controllers.security.PublicAccess;
 import init.DatabaseInitializer;
+import models.contracts.ContractState;
 import models.user.Profile;
 import models.user.User;
 import notifiers.Mails;
 import play.Logger;
 import play.Play;
+import service.ContractService;
 
 import java.util.List;
 
@@ -29,5 +31,46 @@ public class Init extends UtilController {
         }
 
         Dashboard.index();
+    }
+
+    public static void regenerate(String password) {
+
+        ContractService service = new ContractService();
+
+        if (null == password || !password.equals("fofobabar")) {
+            forbidden();
+        }
+
+        for (User u : User.<User>findAll()) {
+
+            if (u.hasContract()) {
+                Logger.info("INIT - Regenerate contract for user : " + u);
+                service.createForUser(u);
+            } else {
+                Logger.info("INIT - No contract to regenerate for user : " + u);
+            }
+        }
+
+        ok();
+    }
+
+    public static void update(String password) {
+
+
+        if (null == password || !password.equals("fofobabar")) {
+            forbidden();
+        }
+
+        User last = User.find("order by staNumber desc limit 1").first();
+
+        User user1 = User.find("byIdBooster", "59032").first();
+        user1.staNumber = last.staNumber + 1;
+        user1.save();
+
+        User user2 = User.find("byIdBooster", "43974").first();
+        user2.staNumber = last.staNumber + 2;
+        user2.save();
+
+        ok();
     }
 }
