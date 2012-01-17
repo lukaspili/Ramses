6d5010d8c496e0a2d7d8257035e9f3e49348d490
Lukas <lukasz.pili@gmail.com>
2012-01-17 16:19:39 +0100

    Prestation comparaison.

--- app/models/school/Prestation.java
+++ app/models/school/Prestation.java
@@ -31,5 +31,29 @@ public class Prestation extends GenericModel {
         return hours * realCourse.yearCourse.type.getPriceForYearPromotion(realCourse.yearPromotion);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
 
+        Prestation that = (Prestation) o;
+
+        if (hours != that.hours) return false;
+        if (jobOrder != null ? !jobOrder.equals(that.jobOrder) : that.jobOrder != null) return false;
+        if (professor != null ? !professor.equals(that.professor) : that.professor != null) return false;
+        if (realCourse != null ? !realCourse.equals(that.realCourse) : that.realCourse != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (professor != null ? professor.hashCode() : 0);
+        result = 31 * result + (realCourse != null ? realCourse.hashCode() : 0);
+        result = 31 * result + (int) (hours ^ (hours >>> 32));
+        result = 31 * result + (jobOrder != null ? jobOrder.hashCode() : 0);
+        return result;
+    }
 }
